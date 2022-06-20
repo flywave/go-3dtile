4204519048b4c20975b7e0243a0863be6071374d
hj <hawking2215@gmail.com>
2022-06-20 13:37:43 +0800

    update

--- gltf_test.go
+++ gltf_test.go
@@ -19,23 +19,22 @@ func TestLoadGlb(t *testing.T) {
 }
 
 func TestReadB3dm(t *testing.T) {
-	f1, _ := os.Open("./data/0.cmpt")
+	f1, _ := os.Open("./data/0-8.i3dm")
 	// bt := make([]byte, 69)
 	// f1.Read(bt)
 	// fmt.Println(string(bt))
-	cp1 := &Cmpt{}
+	cp1 := &I3dm{}
 	cp1.Read(f1)
-	b3d := cp1.Tiles[0].(*B3dm)
-	bts, _ := mst.GetGltfBinary(b3d.Model, 8)
+	bts, _ := mst.GetGltfBinary(cp1.Model, 8)
 	ioutil.WriteFile("./data/0.glb", bts, 0755)
 }
 
 func TestReadi3dm(t *testing.T) {
-	f, _ := os.Open("/home/hj/workspace/GISCore/build/public/Upload/Tilesets/yanshi/11-12/14/13533/6366/0-34.b3dm")
+	f, _ := os.Open("/home/hj/workspace/GISCore/build/public/Upload/Tilesets/yanshi/11-12/14/13533/6366/0-2.i3dm")
 	// bt := make([]byte, 267)
 	// f.Read(bt)
 	// fmt.Println(string(bt))
-	b3d := &B3dm{}
+	b3d := &I3dm{}
 	b3d.Read(f)
 	bts, _ := mst.GetGltfBinary(b3d.Model, 8)
 	ioutil.WriteFile("./data/0.glb", bts, 0755)
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/flywave/go-3dtile
 go 1.16
 
 require (
+	github.com/flywave/go-mst v0.0.0-20220613140218-f06b8ab84daf // indirect
 	github.com/flywave/go3d v0.0.0-20220209071216-2c50e8b3e7ff
 	github.com/qmuntal/gltf v0.20.3
 )
